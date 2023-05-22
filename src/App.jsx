01ffd5086efcda60de5e3063701a1ce0b7d2062a
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2023-05-22 18:36:40 +0300

    Finished basic start page stylling 🎨     - added blue & yellow blob to the screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import blueBlob from "./assets/blue-blob.svg"
+import yellowBlob from "./assets/yellow-blob.svg"
 
 function App() {
-
   return (
-    <>
+    <main className="main">
 
-    </>
-  )
+      <img src={yellowBlob} className="yellow-blob" alt="" />
+      <img src={blueBlob} className="blue-blob" alt="" />
+      <h1>Quizzical</h1>
+      <h3>Interactive quizzes app for enhancing knowledge.</h3>
+      <button className="start--btn">Start quiz</button>
+    </main>
+  );
 }
 
-export default App
+export default App;
